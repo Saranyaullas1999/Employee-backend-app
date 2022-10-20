f97d35ead273003749d22d83283b5ee2629c48f4
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-20 23:35:55 +0530

    employee-backend-app

---
+++ src/main/java/com/nestdigital/employeebackendapp/EmployeeBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.employeebackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/employeebackendapp/EmployeeBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.employeebackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
